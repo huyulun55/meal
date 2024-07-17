a2ad383971a3a6f7c92564b89ef367069264a6ab
huyulun <huyulun@icloud.com>
2024-07-17 11:30:15 +0800

    Changes

--- Meal/main.cpp
+++ Meal/main.cpp
@@ -2,6 +2,9 @@
 
 int main() {
     std::cout << "Please click the button to get the random food list today" << std::endl;
+    std::cout << "chose 1: apple" << std::endl;
+    std::cout << "chose 2: pear" << std::endl;
+    std::cout << "chose 3: strawbeary" << std::endl;
     return 0;
 }
 
